0120531913a76430f78fd9950316adef5a247301
RobertTheDev <robertthedev@gmail.com>
2024-03-18 19:46:21 +0000

    feat: added event view links

--- components/event/EventCard.vue
+++ components/event/EventCard.vue
@@ -1,18 +1,15 @@
 <template>
-    <button @click="goToEvent(event.slug)">
+    <div>
         <p>{{ event.title }}</p>
         <img :src="event.imageUrl" />
-    </button>
+        <NuxtLink :to="`/events/${event.slug}`">Event</NuxtLink>
+        <NuxtLink :to="`/events/${event.slug}/update`">Update Event</NuxtLink>
+        <NuxtLink :to="`/events/${event.slug}/delete`">Delete Event</NuxtLink>
+    </div>
 </template>
 
 <script setup lang="ts">
 import type IEvent from "~/interfaces/Event";
 
 const event = defineProps<IEvent>();
-
-const router = useRouter();
-
-function goToEvent(slug: string) {
-    router.push(`/events/${slug}`);
-}
 </script>
