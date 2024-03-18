be1b99924684e0541ee9c6bd57e5737dce5de904
RobertTheDev <robertthedev@gmail.com>
2024-03-18 17:12:48 +0000

    feat: defined events resolver

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -2,16 +2,19 @@ import { Resolvers } from "#graphql/resolver";
 import { schema } from "#graphql/schema";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateH3Handler } from "@as-integrations/h3";
+import { mongoDb } from "../configs/mongodb";
+
+async function getEvents() {
+    try {
+        return await mongoDb.collection("events").find({}).toArray();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 const resolvers: Resolvers = {
     Query: {
-        books: () => {
-            return [
-                {
-                    title: "GraphQL with Nuxt",
-                },
-            ];
-        },
+        events: () => getEvents(),
     },
 };
 
