f22f7115d9b1506d8654f905b167e1d9bbc249d5
RobertTheDev <robertthedev@gmail.com>
2024-03-18 19:05:11 +0000

    feat: added fundraiser route

--- components/event/EventCard.vue
+++ components/event/EventCard.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
+    <button @click="goToEvent(event.slug)">
         <p>{{ event.title }}</p>
         <img :src="event.imageUrl" />
-    </div>
+    </button>
 </template>
 
 <script setup lang="ts">
 import type IEvent from "~/interfaces/Event";
 
 const event = defineProps<IEvent>();
+
+const router = useRouter();
+
+function goToEvent(slug: string) {
+    router.push(`/events/${slug}`);
+}
 </script>
