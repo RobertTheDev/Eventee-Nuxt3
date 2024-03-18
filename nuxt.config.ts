f90183a73fd04bf26bbb1903f6e449a48890f7ef
RobertTheDev <robertthedev@gmail.com>
2024-03-18 16:02:15 +0000

    feat: installed and set up apollo with nuxt 3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
+import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["@nuxtjs/eslint-module", "@nuxtjs/apollo"],
+    modules: ["@nuxtjs/eslint-module", "@nuxtjs/apollo", "nuxt-graphql-server"],
+    graphqlServer: {
+        schema: "./server/**/*.graphql",
+        url: "/api/graphql",
+    },
 });
