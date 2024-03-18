871a3c7dfff2d87f536cb98f514f414cf0358bb8
RobertTheDev <robertthedev@gmail.com>
2024-03-18 17:12:37 +0000

    feat: fetched events data with apollo client

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-        <NuxtWelcome />
-    </div>
+    <p>There are {{ data?.events.length || 0 }} events.</p>
 </template>
+<script lang="ts" setup>
+interface IEvent {
+    title: string;
+}
+
+const query = gql`
+    query EventsQuery {
+        events {
+            title
+        }
+    }
+`;
+const variables = { limit: 5 };
+const { data } = await useAsyncQuery<{ events: IEvent[] }>(query, variables);
+</script>
