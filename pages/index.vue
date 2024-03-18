a826312177965c01d8c74db077a438972a36816c
RobertTheDev <robertthedev@gmail.com>
2024-03-18 19:05:36 +0000

    feat: mapped event card component

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul>
+        <EventCard
+            v-for="event in data.events"
+            :key="event.title"
+            v-bind="event"
+        />
+    </ul>
+</template>
+
+<script lang="ts" setup>
+import EventCard from "~/components/event/EventCard.vue";
+import type IEvent from "../interfaces/Event";
+
+const query = gql`
+    query EventsQuery {
+        events {
+            imageUrl
+            slug
+            title
+        }
+    }
+`;
+const variables = { limit: 5 };
+const { data } = await useAsyncQuery<{ events: IEvent[] }>(query, variables);
+</script>
