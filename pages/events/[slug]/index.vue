5dbf1df30dd64563d0de116b9e887071daa1d34a
RobertTheDev <robertthedev@gmail.com>
2024-03-18 19:27:06 +0000

    feat: fetched and displayed fundraiser data

--- pages/events/[slug]/index.vue
+++ pages/events/[slug]/index.vue
@@ -1,9 +1,29 @@
 <template>
-    <div>
-        <p>{{ route.params.slug }}</p>
+    <div v-if="data">
+        <p>{{ data.eventBySlug?.title }}</p>
+        <img :src="data.eventBySlug?.imageUrl" />
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import type IEvent from "../../../interfaces/Event";
+
 const route = useRoute();
+
+const { slug } = route.params;
+
+const query = gql`
+    query EventBySlugQuery($slug: String!) {
+        eventBySlug(slug: $slug) {
+            imageUrl
+            slug
+            title
+        }
+    }
+`;
+const variables = { slug };
+const { data } = await useAsyncQuery<{ eventBySlug: IEvent | null }>(
+    query,
+    variables,
+);
 </script>
