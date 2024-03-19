dab9a9794141279faf4d59eb958d8c223ecd2eff
RobertTheDev <robertthedev@gmail.com>
2024-03-19 13:58:08 +0000

    feat: added header links

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -3,6 +3,10 @@
         class="bg-white flex items-center min-w-full px-12 py-5 shadow-md sticky top-0"
     >
         <NuxtLink class="font-bold text-xl" to="/">Eventee</NuxtLink>
+        <nav>
+            <NuxtLink to="/events">Find events</NuxtLink>
+            <NuxtLink to="/events/create">Create event</NuxtLink>
+        </nav>
         <div class="h-8 w-8 rounded-full overflow-hidden">
             <img src="/default-profile-image.jpg" />
         </div>
