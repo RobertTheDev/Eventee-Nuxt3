dffa5cde3d269a5022b5eff76d4179f973a8f57f
RobertTheDev <robertthedev@gmail.com>
2024-03-19 02:51:33 +0000

    feat: added form and page styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col items-center min-h-screen min-w-full">
         <NuxtLoadingIndicator />
         <LayoutHeader />
-        <main class="flex flex-1">
+        <main class="flex flex-1 min-w-full">
             <slot />
         </main>
         <LayoutFooter />
---
+++ pages/auth/sign-up.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg-slate-50 py-16 min-w-full flex flex-col items-center">
+        <form
+            class="bg-white flex flex-col items-center overflow-hidden rounded-xl px-10 py-10 shadow-lg w-2/6"
+        >
+            <p class="text-2xl font-bold mb-8">Sign Up with Eventee</p>
+            <div class="flex flex-col w-full">
+                <label class="font-medium mb-2 text-sm">Email</label>
+                <input
+                    class="border-2 border-lightgray h-12 mb-6 px-2 rounded-md w-full"
+                />
+            </div>
+            <div class="flex flex-col w-full">
+                <label class="font-medium mb-2 text-sm">Password</label>
+                <input
+                    class="border-2 border-lightgray h-12 mb-8 px-2 rounded-md w-full"
+                />
+            </div>
+            <button
+                class="bg-blue-400 font-medium h-12 px-2 rounded-md text-sm w-full"
+            >
+                Sign Up
+            </button>
+        </form>
+    </div>
+</template>
