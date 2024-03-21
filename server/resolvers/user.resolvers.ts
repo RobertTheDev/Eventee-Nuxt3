07498235f57d22c5d1b941e7c2d0a4570e50e67f
RobertTheDev <robertthedev@gmail.com>
2024-03-21 00:56:55 +0000

    fix: fixed import paths

--- server/resolvers/user.resolvers.ts
+++ server/resolvers/user.resolvers.ts
@@ -1,20 +1,7 @@
 import { Resolvers } from "#graphql/resolver";
-import { signIn, signUp } from "../handlers/auths/auth.handlers";
 
 const userResolvers: Resolvers = {
-    Mutation: {
-        signUp: (
-            _root: unknown,
-            args: { input: { email: string; name: string; password: string } },
-        ) => signUp(args.input),
-        signIn: (
-            _root: unknown,
-            args: { input: { email: string; password: string } },
-        ) => signIn(args.input),
-        currentUser: (_root: unknown, args, context) => {
-            return context.user;
-        },
-    },
+    Mutation: {},
 };
 
 export default userResolvers;
