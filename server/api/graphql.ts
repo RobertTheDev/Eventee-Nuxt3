b05361af486db2ba453a8cc9aa0ca1fbd87ef6a7
RobertTheDev <robertthedev@gmail.com>
2024-03-21 00:57:20 +0000

    ci: removed context

--- server/api/graphql.ts
+++ server/api/graphql.ts
@@ -1,23 +1,18 @@
 import { startServerAndCreateH3Handler } from "@as-integrations/h3";
-import { getCurrentUser } from "../handlers/auths/auth.handlers";
+// import { getCurrentUser } from "../handlers/auths/auth.handlers";
 import apollo from "../configs/apollo";
 
 export default startServerAndCreateH3Handler(apollo, {
-    context: (event) => {
-        const bearerToken = event.event.headers.get("authorization");
-
-        if (!bearerToken) {
-            return null;
-        }
-
-        const token = bearerToken.replace("Bearer ", "");
-
-        const user = getCurrentUser({ token });
-
-        if (!user) {
-            return null;
-        }
-
-        return { user };
-    },
+    // context: (event) => {
+    //     const bearerToken = event.event.headers.get("authorization");
+    //     if (!bearerToken) {
+    //         return null;
+    //     }
+    //     const token = bearerToken.replace("Bearer ", "");
+    //     const user = getCurrentUser({ token });
+    //     if (!user) {
+    //         return null;
+    //     }
+    //     return { user };
+    // },
 });
